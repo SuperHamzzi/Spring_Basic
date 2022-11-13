11afc40adb628867f6b005df21959591d788b873
SuperHamzzi <gksrnrrl6gh@naver.com>
2022-11-14 01:03:13 +0900

    11-14 commit

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name); //Null을 반환하지 않고 감싼다.
+    List<Member> findAll();
+
+
+}
